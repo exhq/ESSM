198f9b84e134712746c665c01231599bf8399a56
nea <nea@nea.moe>
2023-08-24 21:02:45 +0200

    Properly scale bottom bar

--- src/main/java/dev/exhq/ScoreboardInfo.java
+++ src/main/java/dev/exhq/ScoreboardInfo.java
@@ -5,20 +5,15 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 
 
-
 public class ScoreboardInfo implements HudRenderCallback {
     @Override
     public void onHudRender(DrawContext drawContext, float tickDelta) {
-        drawContext.getMatrices().push();
-        drawContext.fill(0, (MinecraftClient.getInstance().getWindow().getHeight())-200, MinecraftClient.getInstance().getWindow().getWidth(), MinecraftClient.getInstance().getWindow().getHeight(), -1);
-        drawContext.getMatrices().pop();
-
-
-
+        drawContext.fill(0, (MinecraftClient.getInstance().getWindow().getScaledHeight()) -50,
+                MinecraftClient.getInstance().getWindow().getScaledWidth(), MinecraftClient.getInstance().getWindow().getScaledHeight(), -1);
 
         drawContext.getMatrices().push();
         ESSMhud.PurseAmount.applyTransformations(drawContext.getMatrices());
-        drawContext.drawTextWithShadow(MinecraftClient.getInstance().textRenderer,EchosShittySkyBlockMod.coinLogo, 0,0, 0xfffca800);
+        drawContext.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, EchosShittySkyBlockMod.coinLogo, 0, 0, 0xfffca800);
         drawContext.getMatrices().pop();
     }
 }
